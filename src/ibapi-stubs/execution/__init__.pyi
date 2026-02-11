758e67ded93221aac5691194e7c152411b1ece30
Tony Lee <iamtonylee@gmail.com>
2026-02-10 18:15:54 -0800

    Add stubs for 10.32.01a1

---
+++ src/ibapi-stubs/execution/__init__.pyi
@@ -0,0 +1,36 @@
+from _typeshed import Incomplete
+from ibapi.const import UNSET_DECIMAL as UNSET_DECIMAL
+from ibapi.object_implem import Object as Object
+from ibapi.utils import decimalMaxString as decimalMaxString, floatMaxString as floatMaxString, intMaxString as intMaxString, longMaxString as longMaxString
+
+class Execution(Object):
+    execId: str
+    time: str
+    acctNumber: str
+    exchange: str
+    side: str
+    shares: Incomplete
+    price: float
+    permId: int
+    clientId: int
+    orderId: int
+    liquidation: int
+    cumQty: Incomplete
+    avgPrice: float
+    orderRef: str
+    evRule: str
+    evMultiplier: float
+    modelCode: str
+    lastLiquidity: int
+    pendingPriceRevision: bool
+    def __init__(self) -> None: ...
+
+class ExecutionFilter(Object):
+    clientId: int
+    acctCode: str
+    time: str
+    symbol: str
+    secType: str
+    exchange: str
+    side: str
+    def __init__(self) -> None: ...
---
+++ src/ibapi-stubs/ineligibility_reason/__init__.pyi
@@ -0,0 +1,7 @@
+from _typeshed import Incomplete
+from ibapi.object_implem import Object as Object
+
+class IneligibilityReason(Object):
+    id_: Incomplete
+    description: Incomplete
+    def __init__(self, id_: str = None, description: str = None) -> None: ...
