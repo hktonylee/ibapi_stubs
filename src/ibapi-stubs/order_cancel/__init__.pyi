4aa3769d5d43e10a61b1afbe21c4e1fd94336d8f
Tony Lee <iamtonylee@gmail.com>
2026-02-10 18:15:18 -0800

    Add stubs for 10.30.01a1

---
+++ src/ibapi-stubs/order_cancel/__init__.pyi
@@ -0,0 +1,11 @@
+from _typeshed import Incomplete
+from ibapi.const import UNSET_INTEGER as UNSET_INTEGER
+from ibapi.object_implem import Object as Object
+from ibapi.utils import intMaxString as intMaxString
+
+class OrderCancel(Object):
+    manualOrderCancelTime: str
+    extOperator: str
+    externalUserId: str
+    manualOrderIndicator: Incomplete
+    def __init__(self) -> None: ...
