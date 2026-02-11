9f5072d12bf3f18c16e80bab4b238e45ed309c56
Tony Lee <iamtonylee@gmail.com>
2026-02-10 18:45:26 -0800

    Add stubs for 10.38.01

---
+++ src/ibapi-stubs/scanner/__init__.pyi
@@ -0,0 +1,38 @@
+from _typeshed import Incomplete
+from ibapi.const import UNSET_DOUBLE as UNSET_DOUBLE, UNSET_INTEGER as UNSET_INTEGER
+from ibapi.object_implem import Object as Object
+
+class ScanData(Object):
+    contract: Incomplete
+    rank: Incomplete
+    distance: Incomplete
+    benchmark: Incomplete
+    projection: Incomplete
+    legsStr: Incomplete
+    def __init__(self, contract=None, rank: int = 0, distance: str = '', benchmark: str = '', projection: str = '', legsStr: str = '') -> None: ...
+
+NO_ROW_NUMBER_SPECIFIED: int
+
+class ScannerSubscription(Object):
+    numberOfRows: Incomplete
+    instrument: str
+    locationCode: str
+    scanCode: str
+    abovePrice: Incomplete
+    belowPrice: Incomplete
+    aboveVolume: Incomplete
+    marketCapAbove: Incomplete
+    marketCapBelow: Incomplete
+    moodyRatingAbove: str
+    moodyRatingBelow: str
+    spRatingAbove: str
+    spRatingBelow: str
+    maturityDateAbove: str
+    maturityDateBelow: str
+    couponRateAbove: Incomplete
+    couponRateBelow: Incomplete
+    excludeConvertible: bool
+    averageOptionVolumeAbove: Incomplete
+    scannerSettingPairs: str
+    stockTypeFilter: str
+    def __init__(self) -> None: ...
