7c942773c9cd9d41b017cbb2e1f4cc5fb1c3efac
Tony Lee <iamtonylee@gmail.com>
2026-02-10 18:06:34 -0800

    Add stubs for 10.10.03a1

---
+++ src/ibapi-stubs/errors/__init__.pyi
@@ -0,0 +1,21 @@
+from _typeshed import Incomplete
+
+class CodeMsgPair:
+    errorCode: Incomplete
+    errorMsg: Incomplete
+    def __init__(self, code, msg) -> None: ...
+    def code(self): ...
+    def msg(self): ...
+
+ALREADY_CONNECTED: Incomplete
+CONNECT_FAIL: Incomplete
+UPDATE_TWS: Incomplete
+NOT_CONNECTED: Incomplete
+UNKNOWN_ID: Incomplete
+UNSUPPORTED_VERSION: Incomplete
+BAD_LENGTH: Incomplete
+BAD_MESSAGE: Incomplete
+SOCKET_EXCEPTION: Incomplete
+FAIL_CREATE_SOCK: Incomplete
+SSL_FAIL: Incomplete
+INVALID_SYMBOL: Incomplete
